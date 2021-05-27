885cb046a713fc2c92827432798458ff68e0ad65
Peter <schmidt.multimedia@gmail.com>
2021-05-27 14:31:21 +0200

    view-schicht ++

--- events.js
+++ events.js
@@ -1,18 +1,39 @@
 
-/* Tools */
-function ausgabe(outputStr) {
-    console.log(outputStr);
-}
+
 
 /** Btn */
-let btn;
-btn = document.getElementById("trigBtn");
-btn = document;
-ausgabe(btn);
+let btn = document.getElementById("trigBtn");
+
+//ausgabe(btn);
 
 btn.addEventListener("click",test);
 
 
+/********  Tools **********/
 function test() {
     ausgabe("hi");
+}
+
+
+/********  Änderung in View-Schicht **********/
+
+// Modul: Umschaltung Klasse | Test:
+//switchClassName("night");
+//switchClassName("day");
+function switchClassName(modeStr) {
+    document.body.className = modeStr;
+    document.body.children[0].className = modeStr;
+    document.body.children[1].className = modeStr;
+}
+
+// Modul: Umschaltung BtnTxt | Test:
+// switchBtnTxt("day");
+// switchBtnTxt("night");
+function switchBtnTxt(modeStr) {
+    btn.innerHTML = modeStr;
+}
+
+/* Tools */
+function ausgabe(outputStr) {
+    console.log(outputStr);
 }
